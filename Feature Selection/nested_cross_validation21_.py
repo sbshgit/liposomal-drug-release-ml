f54fbaec57ca39413f07b7ffeaeae6e36921f3c0
Saba shiranirad <131943233+sbshgit@users.noreply.github.com>
2025-05-30 14:05:04 +0330

    Add files via upload

---
+++ Feature Selection/nested_cross_validation21_.py
@@ -0,0 +1,224 @@
+
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GroupKFold, GroupShuffleSplit
+from sklearn.metrics import mean_absolute_error
+
+
+from sklearn.linear_model import LinearRegression
+from sklearn import linear_model
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+from lightgbm import LGBMRegressor
+from ngboost import NGBRegressor
+from sklearn.model_selection import RandomizedSearchCV 
+
+class nested_cross_validation:
+  
+   
+
+    def __init__(self, datafile = "21_input_features.xlsx", model_type = None):
+        self.df = pd.read_excel(datafile)
+          
+        if model_type == 'lasso':
+          self.user_defined_model = linear_model.Lasso()
+          self.p_grid = {'alpha':[0.01, 0.02, 0.05, 0.1, 0.25, 0.5, 1.0],
+                        'positive':[True, False]}
+
+        elif model_type == 'kNN':
+          self.user_defined_model = KNeighborsRegressor()
+          self.p_grid ={'n_neighbors':[2, 4, 5, 6, 8, 10, 12, 15, 20, 25, 30, 50],
+                        'weights': ["uniform", 'distance'],
+                        'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
+                        'leaf_size': [10, 30, 50, 75, 100],
+                        'p':[1, 2],
+                        'metric': ['minkowski']}
+
+        elif model_type == 'PLS':
+          self.user_defined_model = PLSRegression()
+          self.p_grid ={'n_components':[2, 4, 6],
+                        'max_iter': [250, 500, 750, 1000]}
+
+        elif model_type == 'SVR':
+          self.user_defined_model = SVR()
+          self.p_grid ={'kernel':['linear', 'poly', 'rbf', 'sigmoid'],
+                        'degree':[2, 3, 4, 5, 6],
+                        'gamma':['scale', 'auto'],
+                        'C':[0.1, 0.5, 1, 2],
+                        'epsilon':[0.001, 0.005, 0.01, 0.05, 0.1, 0.15, 0.2],
+                        'shrinking': [True, False]}
+        
+        elif model_type == 'DT':
+          self.user_defined_model = DecisionTreeRegressor(random_state=4)
+          self.p_grid ={'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                        'splitter':['best', 'random'],
+                        'max_depth':[None],
+                        'min_samples_split':[2,4,6],
+                        'min_samples_leaf':[1,2,4],
+                        'max_features': [None, 'auto', 'sqrt','log2'],
+                        'ccp_alpha': [0, 0.05, 0.1, 0.15]}  
+        
+        elif model_type == 'RF':
+          self.user_defined_model = RandomForestRegressor(random_state=4)
+          self.p_grid ={'n_estimators':[100,300,400],
+                        'criterion':['squared_error', 'absolute_error'],
+                        'max_depth':[None],
+                        'min_samples_split':[2,4,6,8],
+                        'min_samples_leaf':[1,2,4],
+                        'min_weight_fraction_leaf':[0.0],
+                        'max_features': ['auto', 'sqrt'],
+                        'max_leaf_nodes':[None],
+                        'min_impurity_decrease': [0.0],
+                        'bootstrap':[True],
+                        'oob_score':[True],
+                        'ccp_alpha': [0, 0.005, 0.01]}
+
+        elif model_type == 'LGBM':
+          self.user_defined_model = LGBMRegressor(random_state=4)
+          self.p_grid ={"n_estimators":[100,150,200,250,300,400,500,600],
+                        'boosting_type': ['gbdt', 'dart', 'goss'],
+                        'num_leaves':[16,32,64,128,256],
+                        'learning_rate':[0.1,0.01,0.001,0.0001],
+                        'min_child_weight': [0.001,0.01,0.1,1.0,10.0],
+                        'subsample': [0.4,0.6,0.8,1.0],
+                        'min_child_samples':[2,10,20,40,100],
+                        'reg_alpha': [0, 0.005, 0.01, 0.015],
+                        'reg_lambda': [0, 0.005, 0.01, 0.015]}
+        
+        elif model_type == 'XGB':
+          self.user_defined_model = XGBRegressor(objective ='reg:squarederror')
+          self.p_grid ={'booster': ['gbtree', 'gblinear', 'dart'],
+                        "n_estimators":[100, 150, 300, 400],
+                        'max_depth':[3, 4, 5, 6, 7, 8, 9, 10],
+                        'gamma':[0, 2, 4, 6, 8, 10],
+                        'learning_rate':[0.3, 0.2, 0.1, 0.05, 0.01],
+                        'subsample': [0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+                        'min_child_weight': [1.0, 2.0, 4.0, 5.0],
+                        'max_delta_step':[1, 2, 4, 6, 8, 10],
+                        'reg_alpha':[0.001, 0.01, 0.1],
+                        'reg_lambda': [0.001, 0.01, 0.1]}                
+        
+        elif model_type == 'NGB':
+          b1 = DecisionTreeRegressor(criterion='squared_error', max_depth=2)
+          b2 = DecisionTreeRegressor(criterion='squared_error', max_depth=4)
+          b3 = DecisionTreeRegressor(criterion='squared_error', max_depth=8) 
+          b4 = DecisionTreeRegressor(criterion='squared_error', max_depth=12)
+          b5 = DecisionTreeRegressor(criterion='squared_error', max_depth=16)
+          b6 = DecisionTreeRegressor(criterion='squared_error', max_depth=32) 
+          self.user_defined_model = NGBRegressor()
+          self.p_grid ={'n_estimators':[100,200,300,400,500,600,800],
+                        'learning_rate': [0.1, 0.01, 0.001],
+                        'minibatch_frac': [1.0, 0.8, 0.5],
+                        'col_sample': [1, 0.8, 0.5],
+                        'Base': [b1, b2, b3, b4, b5, b6]}
+        
+        else:
+          print("please select one of these machine learning models: LinearRegression, lasso, KNeighborsRegressor, PLSRegression, SVR, DecisionTreeRegressor, RandomForestRegressor, LGBMRegressor, XGBRegressor, NGBRegressor  ")
+
+    def input_target(self):
+        X = self.df.drop(['Formulation_Number','Drug_Lipid_Group','Release'],axis='columns')
+        stdScale = StandardScaler().fit(X)
+        self.X=stdScale.transform(X)
+        self.Y = self.df['Release']
+        self.G = self.df['Drug_Lipid_Group']
+        self.E = self.df['Formulation_Number']
+        self.T = self.df['Time']    
+    
+    def cross_validation(self, input_value):
+        if input_value == None:
+            NUM_TRIALS = 10
+        else: 
+            NUM_TRIALS = input_value
+
+        self.itr_number = [] # create new empty list for itr number 
+        self.outer_results = []
+        self.inner_results = []
+        self.model_params = []
+        self.G_test_list = []
+        self.y_test_list = []
+        self.E_test_list = []
+        self.T_test_list = []
+        self.pred_list = []
+
+        for i in range(NUM_TRIALS): #configure the cross-validation procedure - outer loop (test set) 
+  
+          cv_outer = GroupShuffleSplit(n_splits=1, test_size=0.2, random_state=i) #hold back 20% of the groups for test set
+
+          # split data using GSS
+          for train_index, test_index in cv_outer.split(self.X, self.Y, self.E):
+              X_train, X_test = self.X[train_index], self.X[test_index]
+              y_train, y_test = self.Y[train_index], self.Y[test_index]
+              G_train, G_test = self.G[train_index], self.G[test_index]
+              E_train, E_test = self.E[train_index], self.E[test_index]
+              T_train, T_test = self.T[train_index], self.T[test_index]
+
+              # store test set information
+              G_test = np.array(G_test) #prevents index from being brought from dataframe
+              self.G_test_list.append(G_test)
+              E_test = np.array(E_test) #prevents index from being brought from dataframe
+              self.E_test_list.append(E_test)
+              T_test = np.array(T_test) #prevents index from being brought from dataframe
+              self.T_test_list.append(T_test)
+              y_test = np.array(y_test) #prevents index from being brought from dataframe
+              self.y_test_list.append(y_test)
+                    
+              # configure the cross-validation procedure - inner loop (validation set/HP optimization)
+              cv_inner = GroupKFold(n_splits=10) #should be 10 fold group split for inner loop
+
+              # define search space
+              search = RandomizedSearchCV (self.user_defined_model, self.p_grid, n_iter=100, verbose=1, scoring='neg_mean_absolute_error', cv=cv_inner,  n_jobs= -1, refit=True) 
+                      
+              # execute search
+              result = search.fit(X_train, y_train, groups= E_train)
+                  
+              # get the best performing model fit on the whole training set
+              best_model = result.best_estimator_
+
+              # get the score for the best performing model and store
+              best_score = abs(result.best_score_)
+              self.inner_results.append(best_score)
+                      
+              # evaluate model on the hold out dataset
+              yhat = best_model.predict(X_test)
+
+              # store drug release predictions
+              self.pred_list.append(yhat)
+
+              # evaluate the model
+              acc = mean_absolute_error(y_test, yhat)
+                      
+              # store the result
+              self.itr_number.append(i+1)
+              self.outer_results.append(acc)
+              self.model_params.append(result.best_params_)
+
+              # report progress at end of each inner loop
+              print('\n################################################################\n\\results:') 
+              print('Iteration '+str(i+1)+' of '+str(NUM_TRIALS)+' runs completed') 
+              print('Test_Score: %.3f, Best_Valid_Score: %.3f, \n\nBest_Model_Params: \n%s' % (acc, best_score, result.best_params_))
+              print("\n################################################################\n ")
+          
+    def results(self, file_path='results.xlsx'):   
+        #create dataframe with results of nested CV
+        list_of_tuples = list(zip(self.itr_number, self.inner_results, self.outer_results, self.model_params, self.G_test_list, self.E_test_list, self.T_test_list, self.y_test_list, self.pred_list))
+        CV_dataset = pd.DataFrame(list_of_tuples, columns = ['Iter', 'Valid Score', 'Test Score', 'Model Parms', 'Drug_Lipid_Group', "Formulation_Number", "Time", 'Experimental_Release', 'Predicted_Release'])
+        CV_dataset['Score_difference'] = abs(CV_dataset['Valid Score'] - CV_dataset['Test Score']) #Groupby dataframe model iterations that best fit the data (i.e., validitaion <= test)
+        CV_dataset.sort_values(by=['Score_difference', 'Test Score'], ascending=True, inplace=True) 
+        CV_dataset = CV_dataset.reset_index(drop=True) # Reset index of dataframe
+         # Save the DataFrame as an Excel file
+        CV_dataset.to_excel(file_path, index=False) 
+        # save the results as a class object
+        self.CV_dataset = CV_dataset
+
+    def best_model(self):
+        # assign the best model paramaters
+        best_model_params = self.CV_dataset.iloc[0,3]
+        # set params from the best model to a class object
+        best_model = self.user_defined_model.set_params(**best_model_params)
+        self.best_model = best_model.fit(self.X, self.Y)
+    
